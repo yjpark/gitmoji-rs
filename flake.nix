1eebffdfc584cc975ad92ad1b03dac9915918ef8
YJ Park <yjpark@gmail.com>
2023-06-22 23:35:39 +0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      /*
+      defaultPackage = (naersk.lib.${system}.override {
+        inherit (fenix.packages.${system}.minimal) cargo rustc;
+      }).buildPackage { src = ./.; };
+       */
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        buildInputs = with pkgs; [
+          pkgconfig
+          openssl
+        ];
+      in with pkgs; {
+        devShell = mkShell {
+          buildInputs = buildInputs;
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      }
+    );
+}
